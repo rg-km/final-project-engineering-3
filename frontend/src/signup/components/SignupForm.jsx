a8069899c2a4014412f738613605c11505b1def3
unknown <1972008@maranatha.ac.id>
2022-06-13 00:24:46 +0700

    add signup feature

---
+++ frontend/src/signup/components/SignupForm.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { FaUserCircle } from 'react-icons/fa'
+
+
+export const SignupForm = ( {role} ) => {
+  return (
+    <div className="bg-white p-10 mt-10 w-1/4">
+      <div className="flex flex-col items-center">
+        <FaUserCircle fontSize={64} />
+        <h2 className="mt-3 text-3xl font-semibold capitalize">{role}</h2>
+      </div>
+      <div className="mt-3 space-y-8">
+        <div className="flex flex-col space-y-3">
+          <label htmlFor="">Username</label>
+          <input type="text" className="border-b border-black outline-none" />
+        </div>
+
+        <div className="flex flex-col space-y-3">
+          <label htmlFor="">Email</label>
+          <input type="email" className="border-b border-black outline-none" />
+        </div>
+
+        <div className="flex flex-col space-y-3">
+          <label htmlFor="">Password</label>
+          <input type="password" className="border-b border-black outline-none" />
+        </div>
+        <div>
+          <button className="w-full py-3 bg-black rounded-full text-white">Daftar</button>
+        </div>
+      </div>
+    </div>
+  )
+}
