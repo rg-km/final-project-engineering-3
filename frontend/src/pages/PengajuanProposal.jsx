c6c48b1b0e5d82fa5f158561686b17ec9f9d3bb1
Richardo <richardoehbet221@gmail.com>
2022-06-24 19:27:40 +0700

    feat :  consume api list proposal status

--- frontend/src/pages/PengajuanProposal.jsx
+++ frontend/src/pages/PengajuanProposal.jsx
@@ -1,45 +1,31 @@
 import React from 'react'
-import { useState } from 'react'
 import AjuanProposal from '../components/shared/AjuanProposal'
+import Spinner from '../components/shared/Spinner'
+import useFetchData from '../hooks/useFetchData'
 
 const PengajuanProposal = () => {
-    const [ajuan, setAjuan] = useState([
-        {
-            namaResearch: 'nama research 1',
-            bidang: 'bidang 1',
-            namaMitra: 'nama mitra 1',
-            periode: '2022 - 2025',
-            status: 'Pending'
-        },
-        {
-            namaResearch: 'nama research 2',
-            bidang: 'bidang 2',
-            namaMitra: 'nama mitra 2',
-            periode: '2022 - 2026',
-            status: 'Diterima'
-        },
-        {
-            namaResearch: 'nama research 3',
-            bidang: 'bidang 3',
-            namaMitra: 'nama mitra 3',
-            periode: '2022 - 2026',
-            status: 'Ditolak'
-        }        
-    ])
+  const { response, isFetching } = useFetchData(null, '/researcher/proposal')
+
+  if (isFetching || !response) return <Spinner className="mt-10 w-10 h-10" />
   return (
-    <div className='container'>
-    <h1 className="text-2xl font-semibold">Daftar Pengajuan Proposal</h1>
-    {ajuan.map(item =>{
-        return <AjuanProposal
-        key = {item.id}
-        namaResearch = {item.namaResearch}
-        bidang = {item.bidang}
-        namaMitra = {item.namaMitra}
-        periode = {item.periode}
-        status = {item.status}
-        />
-    })}
-</div>
+    <div className="container">
+      <h1 className="text-2xl font-semibold">Daftar Pengajuan Proposal</h1>
+      {response.data &&
+        response.data.map((item) => {
+          return (
+            <AjuanProposal
+              key={item.id}
+              namaResearch={item.challenge_name}
+              bidang={item.research_category}
+              namaMitra={item.industry_name}
+              periodeStart={item.period_start}
+              periodeEnd={item.period_end}
+              status={item.funding_status}
+            />
+          )
+        })}
+      {!response.data && <div className="mt-10 text-center">Anda belum mengajukan proposal</div>}
+    </div>
   )
 }
 
