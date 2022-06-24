a2428b211ec09167acbdbea05866f1a9364cc8b9
Richardo <richardoehbet221@gmail.com>
2022-06-24 10:52:26 +0700

    feat : consume api list challenge

--- frontend/src/components/shared/ChallangeListItem.jsx
+++ frontend/src/components/shared/ChallangeListItem.jsx
@@ -3,10 +3,9 @@ import { AiOutlineUser } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 import { MdOutlineArrowForwardIos } from 'react-icons/md'
 
-
-const ChallangeListItem = ({namaResearch, bidang, namaMitra, periode}) => {
+const ChallangeListItem = ({ id, namaResearch, bidang, namaMitra, periode }) => {
   return (
-    <div className="space-y-3 mt-10">
+    <div className="space-y-3 mt-5">
       <div className="border-2 bg-gray-100 rounded-md p-3 grid grid-cols-[1fr_2fr] md:grid-cols-[1fr_6fr_1fr] gap-3">
         <div className="flex items-center justify-center">
           <AiOutlineUser fontSize={56} />
@@ -18,10 +17,10 @@ const ChallangeListItem = ({namaResearch, bidang, namaMitra, periode}) => {
           <div className="text-xs md:text-sm mb-2">{periode}</div>
         </div>
         <div className="hidden md:flex items-center justify-center">
-        <Link to="somethingId">
-          <MdOutlineArrowForwardIos fontSize={48} />
-        </Link>
-      </div>
+          <Link to={`${id}`}>
+            <MdOutlineArrowForwardIos fontSize={48} />
+          </Link>
+        </div>
       </div>
     </div>
   )
